f7adb492e50a822e555274354f9e502963aeb722
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-08-12 15:51:54 +0100

    Update footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <Flex direction={"column"} alignItems={"center"} pb={5} px={3} gap={1}>
             <Text fontWeight={"bold"}>
                 Built with ❤️ by{" "}
-                <Link as={NextLink} href={"https://twitter.com/EridianAlpha"} color={"blue"} textDecoration={"underline"} target="_blank">
+                <Link as={NextLink} href={"https://eridianalpha.com"} color={"blue"} textDecoration={"underline"} target="_blank">
                     Eridian
                 </Link>
             </Text>
